5fea287e827dec32687e3d66e6a31999c5d1ce02
Staci <59322333+msjupiter96@users.noreply.github.com>
2022-07-16 01:58:26 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
                 <ul className='footer-items'>
                     <li>
                         <div className='logo-list'>
-                            <a href="www.linkedin.com/in/sfraley-developer"><img src='/images/logos_linkedin-icon.png' alt="LinkedIn profile link"></img></a>
+                            <a href="www.linkedin.com/in/sfraley-developer"><img src='images/logos_linkedin-icon.png' alt="LinkedIn profile link"></img></a>
                             <a href="https://github.com/msjupiter96"><img src='images/akar-icons_github-fill.png' alt="Github profile link"></img></a>
                             <a href="mailto:fraleysn@gmail.com?subject=Message from Fishing App"><img src='images/carbon_email.png' alt="Email link"></img></a>
                         </div>
